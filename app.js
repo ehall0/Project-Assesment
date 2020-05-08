d76c6cac84d313e751f73bbd79358fcafe7c6487
Emma Hall <osboxes@osboxes.com>
2020-05-08 11:54:34 -0400

    added red color for negatives

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const add = () => {
    $Result += parseInt($('.input').val())  ;
     $('#result').empty();
     $('#result').append($Result);
-   
+   if(parseInt($Result) < 0){
+       $('#result').css('color', 'red');
+   }else{
+       $('#result').css('color', 'black');
+   }
 };
 
 const sub = () => {
       $Result -= parseInt($('.input').val())  ;
     $('#result').empty();
     $('#result').append($Result);
+    if(parseInt($Result) < 0){
+        $('#result').css('color', 'red');
+    }else{
+        $('#result').css('color', 'black');
+    }
 };
 
 $('#add').on('click',add);
